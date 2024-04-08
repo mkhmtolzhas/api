ef584c4fbd1e47a0be6849313127a72620f737ad
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-04-08 23:45:34 +0500

    isdjad

--- vercel.py
+++ vercel.py
@@ -3,4 +3,4 @@ from os import getenv
 
 if __name__ == "__main__":
     port = int(getenv("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
